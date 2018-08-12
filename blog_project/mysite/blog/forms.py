78c9f605e514df4fa0a12644c86f7af878875a22
ronb <ronbnj@gmail.com>
2018-08-11 23:55:17 -0400

    Added signup flow

--- blog_project/mysite/blog/forms.py
+++ blog_project/mysite/blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from blog.models import Post,Comment
 
 class PostForm(forms.ModelForm):
@@ -18,3 +20,12 @@ class CommentForm(forms.ModelForm):
             'author':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
         }
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=40, required=True, help_text='Required')
+    last_name = forms.CharField(max_length=40, required=True, help_text='Required')
+    email = forms.EmailField(max_length=254, help_text='Required')
+    
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
